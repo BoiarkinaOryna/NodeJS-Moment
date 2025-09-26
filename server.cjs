c1f7ffcc45f771fb51c08fea7564dd3a0ffcd8e0
BoiarkinaOryna <boyarkina.ar@gmail.com>
2025-09-26 22:46:32 +0300

    posts filter & find one by id

--- server.cjs
+++ server.cjs
@@ -1,6 +1,3 @@
-// import express from 'express'
-// import path from 'path'
-// import fs from 'fs'
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
@@ -19,7 +16,40 @@ app.get("/timestamp", (req, res) =>{
 })
 
 app.get("/posts", (req, res) =>{
+    const take = req.query.take
+    const skip = req.query.skip
+
+    console.log("take =", take, "skip =", skip)
+    
+    if (isNaN(take) && take != undefined){
+        res.status(400).json("take must be a number")
+        return
+    } else if(isNaN(skip) && skip != undefined){
+        res.status(400).json("skip must be a number")
+        return
+    }
+    if(take && skip){
+        res.json(neededPosts.slice(skip, +skip + +take))
+        return
+    }else if(take){
+        res.json(neededPosts.slice(0, take))
+        return
+    }else if(skip){
+        res.json(neededPosts.slice(skip))
+        return
+    }
     res.json(allPosts)
+})
+
+app.get("/posts/:id", (req, res) =>{
+    const posts = [...allPosts]
+    posts.forEach((post) =>{
+        if (post.id == req.params.id){
+            res.json(post)
+            return
+        }
+    })
+    res.status(404).json("Post is not found")
 })
 
 app.listen(8000, 'localhost', ()=>{
